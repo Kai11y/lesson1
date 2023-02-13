d4cf50746f91ec06dec3c1dc4ec4d6cbc3566e95
Anastasiya <i@akostina.ru>
2023-02-13 19:19:34 +0300

    Правки по ревью

--- price.py
+++ price.py
@@ -1,18 +1,20 @@
 def get_summ(one, two, delimiter='&'):
-        one = str(one)
-        two = str(two)
-        one_with_two = one + delimiter + two
+    one = str(one)
+    two = str(two)
+    one_with_two = one + delimiter + two
         
-        print(one_with_two)
+    return one_with_two
 
-get_summ(1,2)
+print(get_summ(1,2))
 
-def get_summ(Learn, python):
-        learn_python = Learn + ' ' + python
 
-        print(learn_python)
+def get_summ(learn, python):
+    learn_python = learn + ' ' + python
+
+    print(learn_python)
 
 get_summ('learn'.capitalize(), 'python')
+
 
 def format_price(price):
     price = int(price)
